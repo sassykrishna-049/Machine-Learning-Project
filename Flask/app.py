3f18eaafa787a34294df701f7802cbacefc7ec17
sassykrishna-049 <sahookrishna2@gmail.com>
2023-02-16 20:07:03 +0530

    save the recent chaanges

---
+++ Flask/app.py
@@ -0,0 +1,10 @@
+from Flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "Starting Machine Learning Project"
+
+if __name__=="__main__":
+    app.run(debug=True)
